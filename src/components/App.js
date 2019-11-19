67c9570c596a927d0f648100859903572d2cf3e4
Sam Jespersen <heyheysamj@gmail.com>
2019-11-18 17:34:47 -0800

    further redux scaffolding

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import DisplayCharacters from '../containers/displayCharacters';
+
+export default function App() {
+  return (
+    <DisplayCharacters />
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './components/App'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import store from './store';
 
 render(
-    <App />,
-    document.getElementById('root')
-)
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
