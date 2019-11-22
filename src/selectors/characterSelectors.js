8964f0e649c2922e0ac6dfd17459a6e8c6982a54
Sam Jespersen <heyheysamj@gmail.com>
2019-11-21 22:57:49 -0800

    deleted files

--- src/selectors/characterSelectors.js
+++
@@ -1,3 +0,0 @@
-export const getCharacters = state => state.characters.characters;
-export const getLoading = state => state.characters.loading;
-export const getChar = state => state.characters.detailCharacter;
